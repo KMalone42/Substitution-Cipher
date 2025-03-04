630c10bfa4c7ab76c46af659cb31b205eaeda717
Kristin Malone <kmalone42@outlook.com>
2025-03-04 15:44:40 -0500

    cli project

--- main.py
+++ main.py
@@ -10,6 +10,24 @@ class CLI(cmd.Cmd):
     def reinit_key(self):
         self.key = [''] * 26
 
+    def do_usekey(self, arg):
+        # argument is the key that the user wants to use written as string
+        potential_key = list(arg)
+        if len(potential_key) != 26:
+            print(f"{arg}\nis not a valid key, key length should be = to 26")
+        for char in potential_key:
+            index = potential_key.index(char)
+            # Just gonna do a switch statement i guess
+            if char not in self.alphabet:
+                print(f"{char} is not a valid input")
+                print("input characters must be lowercase and apart of the standard english alphabet")
+                return
+            elif potential_key.count(char) > 1:
+                print(f"{char} is not valid")
+                print("duplicate keypairs not accepted.")
+                return
+        self.key = potential_key
+
     def do_keygen(self, arg):
         if '-m' in arg:
             self.keygen_manual()
@@ -17,6 +35,7 @@ class CLI(cmd.Cmd):
             self.keygen()
 
     def keygen(self):
+        self.reinit_key()
         # Randomly generates your key
         start_time = time.perf_counter()
         for index, letters in enumerate(self.alphabet):
@@ -59,21 +78,61 @@ class CLI(cmd.Cmd):
                 continue
             valid = True
             return inp
-                
-
 
     def do_whatiskey(self, arg):
         try:
             if '' in self.key:
                 print("Key not initialized try 'keygen' or 'keygen -m' for manual key generation")
             else:
-                print("Substitution Key:")
                 print(''.join(map(str, self.key)))
         except:
             print("Key not initialized try 'keygen' or 'keygen -m' for manual key generation")
+    
+    def whatiskey(self):
+        try:
+            if '' in self.key:
+                print("Key not initialized try 'keygen' or 'keygen -m' for manual key generation")
+                return False
+            else:
+                print(''.join(map(str, self.key)))
+                return True
+        except:
+            print("Key not initialized try 'keygen' or 'keygen -m' for manual key generation")
+            return False
+
+
+    # encrypt a message using the key
+    def do_encrypt(self, arg):
+        # obligatory check for a key being loaded
+        if not self.whatiskey():
+            return
+        print(''.join(self.alphabet))
+        message = list(arg)
+        encoded_message = []
+        for char in message:
+            index = self.alphabet.index(char)
+            encoded_message.append(self.key[index])
+        
+        print(''.join(map(str, encoded_message)))
+
+
+    
+    # decrypt a message using the key
+    def do_decrypt(self, arg):
+        # obligatory check for a key being loaded
+        if not self.whatiskey():
+            return
+        print(''.join(self.alphabet))
+        message = list(arg)
+        decoded_message = []
+        for char in message:
+            index = self.key.index(char)
+            decoded_message.append(self.alphabet[index])
+        
+        print(''.join(map(str, decoded_message)))
 
 # runtime starts here
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if __name__ == '__main__':
     try:
         CLI().cmdloop()
